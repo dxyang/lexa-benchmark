c2ffb333d27ae5513c8b76f694e1624e9c68a844
Daniel Yang (CSAIL - Patagonia) <daniel.yang17@gmail.com>
2023-01-06 13:03:14 -0500

    debugging kitchen on supercloud:

--- test.py
+++ test.py
@@ -1,30 +1,53 @@
+import time
+
 import cv2
 import numpy as np
 
 from lexa_envs.kitchen import KitchenEnv
 
-env = KitchenEnv()
-env.reset()
+def run():
+    env = KitchenEnv()
+    env.reset()
 
-print(f"===== Observation Space =====")
-print(env.observation_space)
-print(f"===== Action Space =====")
-print(env.action_space)
+    print(f"===== Observation Space =====")
+    print(env.observation_space)
+    print(f"===== Action Space =====")
+    print(env.action_space)
 
-print(f"===== Simulating =====")
-for i in range(1000):
-    obs, total_reward, done, info = env.step(env.action_space.sample())
-    if i == 0:
-        for k, v in obs.items():
-            if type(v) == np.ndarray:
-                print(f"{k}: {v.shape}")
-            else:
-                print(f"{k}: {type(v)}")
-    # env.render(mode="human")
+    print(f"===== Simulating =====")
+    start = time.time()
+    for i in range(1000):
+        obs, total_reward, done, info = env.step(env.action_space.sample())
+        if i == 0:
+            for k, v in obs.items():
+                if type(v) == np.ndarray:
+                    print(f"{k}: {v.shape}")
+                else:
+                    print(f"{k}: {type(v)}")
+        # env.render(mode="human")
 
-    cv2.imwrite("start_image.png", cv2.cvtColor(obs["image"], cv2.COLOR_BGR2RGB))
-    cv2.imwrite("goal_image.png", cv2.cvtColor(obs["image_goal"], cv2.COLOR_BGR2RGB))
-    import pdb; pdb.set_trace()
-    same_image = np.allclose(obs["image"], obs["image_goal"])
-    if done:
-        print(f"hurrah")
+        # cv2.imwrite("start_image.png", cv2.cvtColor(obs["image"], cv2.COLOR_BGR2RGB))
+        # cv2.imwrite("goal_image.png", cv2.cvtColor(obs["image_goal"], cv2.COLOR_BGR2RGB))
+        # same_image = np.allclose(obs["image"], obs["image_goal"])
+        if done:
+            print(f"hurrah")
+        if i % 100 == 0 and i > 0:
+            now = time.time()
+            print(f"{i} steps done in {now - start} seconds. {(now - start) / i} fps")
+
+    end = time.time()
+    print(f"1000 steps done in {end - start} seconds. {(end - start) / 1000.0} fps")
+    print(f"finished!")
+
+if __name__ == "__main__":
+    supercloud = False
+    if supercloud:
+
+        import jaynes
+
+        jaynes.config()
+        jaynes.add(run)
+        jaynes.execute()
+        jaynes.listen()
+    else:
+        run()
